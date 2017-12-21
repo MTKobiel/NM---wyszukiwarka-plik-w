308c9f4b7bc478fa6ff94f344a2701106fcd726f
rockstarisafk <rockstarisafk@gmail.com>
2017-12-21 19:27:07 +0100

    Utworzenie przycisków

--- NM - wyszukiwarka plików/Form1.cs
+++ NM - wyszukiwarka plików/Form1.cs
@@ -16,5 +16,10 @@ namespace NM___wyszukiwarka_plików
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("tutaj bedzie fajny program", "important message");
+        }
     }
 }
